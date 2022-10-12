6f6c2d1bbd7d4c1c9da0fa8c70db6d3b4e9fe06e
tanzil321 <tanzilwhatever@gmail.com>
2022-10-12 17:44:32 +0600

    Error Page Added

--- src/Components/QueSet.js
+++ src/Components/QueSet.js
@@ -1,14 +1,19 @@
 import React, { useContext } from 'react';
-import OptionSet from './OptionSet';
+
 import { RingContext } from './Topics';
 
 const QueSet = ({que}) => {
-    let far = {listStyle:"decimal", listStylePos:"inside"}
+    let funf = ()=> {
+        // if ans === que{
+            
+        // }
+    }
+    let far = {listStyle:"decimal", listStylePosition:"inside"}
 
     console.log(que.options);
     return (
         <div>
-            <li  style={far}  className='border bg-cyan-200 px-5 py-6 m-auto'>{que}</li>
+            <li onClick={()=>{funf(que)}} style={far}  className='border bg-cyan-200 px-5 py-6 m-auto'>{que}</li>
             
         </div>
     );
