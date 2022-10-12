3611b360f76b37c96564a71975e5c9ad4a58cb44
tanzil321 <tanzilwhatever@gmail.com>
2022-10-12 13:49:47 +0600

    Quiz Section Updated

--- src/Components/Static.js
+++ src/Components/Static.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Static = () => {
     const totalData = useLoaderData();
@@ -8,12 +8,16 @@ const Static = () => {
         <div>
             <h1 className='mt-6 text-center text-5xl font-mono font-bold text-bkack '>Quiz Static Results</h1>
             <div>
+                
+            <ResponsiveContainer  width="90%" height={400}>
             <LineChart className='mt-6 px-4 py-16 mx-auto' width={500} height={400} data={totalData.data}>
             <Line type="monotone" dataKey="total" stroke="#82ca9d" />
             <XAxis dataKey="name"></XAxis>
             <YAxis></YAxis>
             <Tooltip></Tooltip>
             </LineChart>
+            </ResponsiveContainer>
+            
             
             </div>
             
